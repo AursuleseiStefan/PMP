e6bb31c830a2d4286a1610f98f238200728642a7
AursuleseiStefan <40248249+AursuleseiStefan@users.noreply.github.com>
2022-09-30 11:31:21 +0300

    Create distributia gaussiana.py

---
+++ Lab1/distributia gaussiana.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+u = 5
+std = 1
+snd = stats.norm(u, std)
+
+x = np.linspace(-6, 6, 100)
+
+plt.figure(figsize=(8,8))
+plt.plot(x, snd.pdf(x))
+plt.xlim(0, 10)
+plt.title('Distributia Gaussiana', fontsize='15')
+plt.xlabel('Values of Random Variable X', fontsize='15')
+plt.ylabel('Probability', fontsize='15')
+plt.show()
