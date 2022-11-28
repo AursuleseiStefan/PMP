1a2a19e6357349746cb37fead8e8c04335611c23
AursuleseiStefan <40248249+AursuleseiStefan@users.noreply.github.com>
2022-11-28 22:11:59 +0200

    Subpunctul 1 2 si 3

--- Lab 8/lab8.py
+++ Lab 8/lab8.py
@@ -46,3 +46,26 @@ if __name__=="__main__":
     az.plot_hdi(x_1[:,0], idata_1.posterior['bd'],hdi_prob=0.94, color='k')
     plt.xlabel(x_n[0])
     plt.ylabel(x_n[1])
+    
+## subpct 3
+    with pm.Model() as model1:
+        alpha = pm.Normal('alpha', mu=0, sd=10)
+        beta = pm.Normal('beta', mu=0, sd=2, shape=len(x_n))
+
+        mu = alpha + pm.math.dot([550, 3.5], beta)
+        theta = pm.Deterministic('theta', 1 / (1 + pm.math.exp(-mu)))
+        bd = pm.Deterministic('bd', -alpha / beta[1] - beta[0] / beta[1] * 550)
+
+        yl1 = pm.Bernoulli('yl1', p=theta, observed=y_1)
+
+        idata1 = pm.sample(2000, target_accept=0.9, return_inferencedata=True)
+
+    idx = np.argsort(x_1[:, 0])
+    bd = idata1.posterior['bd'].mean(("chain", "draw"))[idx]
+    plt.scatter(x_1[:, 0], x_1[:, 1], c=[f'C{x}' for x in y_1])
+    plt.plot(x_1[:, 0], bd, color='k')
+    az.plot_hdi(x_1[:, 0], idata1.posterior['bd'], hdi_prob=0.9, color='k')
+    plt.xlabel(x_n[0])
+    plt.ylabel(x_n[1])
+    plt.show()
+
